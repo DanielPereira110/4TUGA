ad7b1817948695850ed36040e4cc86732f4d5181
Diogo <diogo.ramalho117@gmail.com>
2015-11-02 19:25:26 +0000

    inicio

---
+++ 4Tuga/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_4Tuga.MvcApplication" Language="C#" %>
---
+++ 4Tuga/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4Tuga.Startup))]
+namespace _4Tuga
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
