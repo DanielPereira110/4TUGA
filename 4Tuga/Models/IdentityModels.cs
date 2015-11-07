e1dac0cfdafb46d5affca006bf9753b9132c1003
Diogo <diogo.ramalho117@gmail.com>
2015-11-07 18:32:42 +0000

    UserDetails + name on LoginPartial

--- 4Tuga/Models/IdentityModels.cs
+++ 4Tuga/Models/IdentityModels.cs
@@ -14,6 +14,7 @@ namespace _4Tuga.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public const string NameClaimType = "Name";
 
         public string Name { get; set; }
         public string Gender { get; set; }
@@ -27,6 +28,7 @@ namespace _4Tuga.Models
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            userIdentity.AddClaim(new Claim(NameClaimType, Name));
             return userIdentity;
         }
     }
