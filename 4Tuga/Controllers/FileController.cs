d9feb0e91521cce099bc99020e66bc68ddfbe7a4
Diogo <diogo.ramalho117@gmail.com>
2015-11-16 22:57:34 +0000

    imagem na criação do user

---
+++ 4Tuga/Controllers/FileController.cs
@@ -0,0 +1,17 @@
+﻿using _4Tuga.DAL;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class FileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //
+        // GET: /File/
+        public ActionResult Index(int id)
+        {
+            var fileToRetrieve = db.Files.Find(id);
+            return File(fileToRetrieve.Content, fileToRetrieve.ContentType);
+        }
+    }
+}
